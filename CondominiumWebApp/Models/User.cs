a7f12a41736fbac9e45cc2d6de234d444a88193b
Hernan Gonzalez <89197163+Hegon90@users.noreply.github.com>
2023-07-23 14:15:00 -0500

    Small Update

--- CondominiumWebApp/Models/User.cs
+++ CondominiumWebApp/Models/User.cs
@@ -8,14 +8,18 @@ public partial class User
 {
     [Key]
     public int UserId { get; set; }
-
+    [Display(Name = "Nombre de Usuario")]
+    [Required(ErrorMessage = "This field is required.")]
     public string UserNickname { get; set; } = null!;
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "This field is required.")]
     public string UserName { get; set; } = null!;
-
+    [Display(Name = "Apellido")]
     public string? UserSurname { get; set; }
-
+    [Display(Name = "Correo")]
+    [Required(ErrorMessage = "This field is required.")]
     public string UserEmail { get; set; } = null!;
-
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "This field is required.")]
     public string UserPassword { get; set; } = null!;
 }
